cdba8d352be703014fd48af564461a0d3ff66fef
AlejandroMolinosEligio <alejandromoeli@gmail.com>
2022-03-18 12:25:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Package L04
+This is my first package
+
+## Install
+```
+npm install
+```
+
+## Start
+```
+npm start
+```
+
+Link a POSTMAN L06 [https://documenter.getpostman.com/view/20091922/UVsPP4n9](https://documenter.getpostman.com/view/20091922/UVsPP4n9)
+
+Done by [@pafmon](www.us.es)
+
+
---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 8080;
+
+const BASE_API_URL = "/api/v1";
+const URL_PORTAL = "https://documenter.getpostman.com/view/20091922/UVsPP4n9";
+
+var contats = [
+    {
+        name: "peter",
+        phone: 12345
+    },
+    {
+        name: "paul",
+        phone: 6789
+    }
+]
+
+app.get(BASE_API_URL+"/contacts",(req, res)=>{
+    res.send(JSON.stringify(contats,null,2));
+})
+
+app.get(BASE_API_URL+"/docs",(req,res)=>
+{
+    res.redirect(URL_PORTAL)
+})
+
+app.get(BASE_API_URL+"/contacts/:name",(req, res)=>{
+
+    var nombre =req.params.name
+    var filteredContatcs = contats.filter((contat)=>
+    {
+        return (contat.name == nombre);
+    });
+    if (filteredContatcs==0){
+        res.sendStatus(404, "NO EXISTE");
+    }else{
+        res.send(JSON.stringify(filteredContatcs,null,2));
+    }
+})
+
+app.post(BASE_API_URL+"/contacts",(req, res)=>{
+    contats.push(req.body);
+    res.sendStatus(201,"CREATED");
+})
+
+app.delete(BASE_API_URL+"/contacts/:name",(req, res)=>{
+    var nombre = req.params.name;
+    contats = contats.filter((contact)=>{
+        return (contact.name != nombre);
+    })
+    res.sendStatus(200,"DELETED");
+})
+
+app.listen(port, () => {
+    console.log(`Server ready at port ${port}`);
+});
